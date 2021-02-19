9cf79568540aa0c1402d943a50e58453bc993ad7
eunsugpark <61435949+eunsugpark@users.noreply.github.com>
2021-02-19 03:10:51 -0600

    Add files via upload

--- tsplotA.m
+++ tsplotA.m
@@ -1,61 +1,48 @@
+function tsplotA(ASnor,alpha)
 %--------------------------
-%tsplotA.m:
-%Construct time-seires plots of source contributions (A) along with 95% posterior intervals.
-%--------------------------------------------------
-%Time series plots of source contributions (A) along with 95% posterior intervals
+% function name: tsplotA
+%
+% Construct time seires plots of source contributions (A) 
+% along with uncertainty estimates (95% posterior intervals).
+%
+% Usage example:
+% tsplotA(ASnor,alpha);
+%
+% Inputs:
+% ASnor: Stored MCMC samples for A (T by q by nsample)
+% (T=number of observations (times), q=number of sources, nsample=number of stored samples)
+% alpha: Significance level, e.g., 0.05
+%
+% Eun Sug Park, August 2020
+% Last updated February 2021
 %---------------------------------------------------
 
-function TSA= tsplotA(Ahat, LPI_A95,UPI_A95)
- 
-YS=Ahat;%input('Ahat=');  %Estiamted T by q source contribution matrix
-[T,q]=size(YS);
+[T,q,nsample]=size(ASnor);
+index_L=floor(alpha/2*nsample);
+index_U=ceil((1-alpha/2)*nsample);
+
+vecAS=reshape(ASnor,T*q,nsample)'; %nsample by Tq
+vmeanA=mean(vecAS);
+Ahat=reshape(vmeanA,T,q);
+
+sortedAnor=sort(vecAS);
+PI_A=sortedAnor([index_L index_U],:);
+LPI_A=reshape(PI_A(1,:),T,q);  %Lower limit of PI for A
+UPI_A=reshape(PI_A(2,:),T,q);  %Upper limit of PI for A
 
 figure
-subplot(q,1,1)
-plot((1:T),YS(:,1),'-',(1:T),LPI_A95(:,1),'r-.',(1:T),UPI_A95(:,1),'r-.')
-axis tight
-legend('Source 1','95% PI','Location','North')
-%title('Source 1')
-
-subplot(q,1,2)
-%plot(YS(:,5))
-plot((1:T),YS(:,2),'-',(1:T),LPI_A95(:,2),'r-.',(1:T),UPI_A95(:,2),'r-.')
-axis tight
-ylabel('Mass concentration')
-legend('Source 2','Location','North')
-
-subplot(q,1,3)
-%plot(YS(:,3))
-plot((1:T),YS(:,3),'-',(1:T),LPI_A95(:,3),'r-.',(1:T),UPI_A95(:,3),'r-.')
-axis tight
-legend('Source 3','Location','North')
-if q>3
-    subplot(q,1,4)
-    %plot(YS(:,1))
-    plot((1:T),YS(:,4),'-',(1:T),LPI_A95(:,4),'r-.',(1:T),UPI_A95(:,4),'r-.')
+for k=1:q
+    subplot(q,1,k)
+    plot((1:T),Ahat(:,k),'-',(1:T),LPI_A(:,k),'r-.',(1:T),UPI_A(:,k),'r-.')
     axis tight
-    if q==4
+    legend(['Source ',num2str(k)],'Location','North')   
+    if k==1
+        legend('Source 1','95% PI','Location','North')
+        title('Source Contributions')
+    elseif k==2
+        ylabel('Mass concentration')
+    elseif k==q
         xlabel('Observation #')
-    end
-    legend('Source 4','Location','North')
-    
-    if q>4
-        subplot(q,1,5)
-        plot((1:T),YS(:,5),'-',(1:T),LPI_A95(:,5),'r-.',(1:T),UPI_A95(:,5),'r-.')
-        axis tight
-        if q==5
-            xlabel('Observation #')
-        end
-        
-        legend('Source 5','Location','North')
-        
-        if q>5
-            subplot(q,1,6)
-            plot((1:T),YS(:,6),'-',(1:T),LPI_A95(:,6),'r-.',(1:T),UPI_A95(:,6),'r-.')
-            axis tight
-            xlabel('Observation #')
-            legend('Source 6','Location','North')
-        end
     end
 end
 
