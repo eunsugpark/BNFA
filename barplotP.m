9770077a26369b4c91248ae7a1abb4bf4bc6dc1e
eunsugpark <61435949+eunsugpark@users.noreply.github.com>
2021-02-19 03:09:42 -0600

    Add files via upload

--- barplotP.m
+++ barplotP.m
@@ -1,54 +1,65 @@
+function barplotP(PSnor,alpha,variable_name)
 %--------------------------------------------
-%barplotP.m:
-%Draw barplots for elements of source profiles (P)
-%with error bars (lower and upper limits of 95% posterior intervals).
+% function name: barplotP
 %
-%Eun Sug Park, August 2020
+% Draw barplots for elements of source profiles (P)
+% with error bars (lower and upper limits of 95% posterior intervals).
+%
+% Usage example:
+% barplotP(PSnor,alpha,variable_name);
+%
+% Inputs:
+% PSnor: Stored MCMC samples for P (q by J by nsample)
+% (q=number of sources, J=number of variables, nsample=number of stored samples)
+% alpha: Significance level, e.g., 0.05
+% variable_name (optional): J-dimensional string vector containing variable names 
+%
+% Eun Sug Park, August 2020
+% Last updated February 2021
 %----------------------------------------
-function Pbar= barplotP(P,LL,UL,variable_name)
-%LL: Lower limit of error bar
-%UL: Upper limit of error bar
-%variable_name: J-dimensional vector containing variable names (optional)
-[q,J]=size(P);
+
+[q,J,nsample]=size(PSnor);
+index_L=floor(alpha/2*nsample);
+index_U=ceil((1-alpha/2)*nsample);
+
+vecPS=reshape(PSnor,q*J,nsample)'; %nsample by qJ
+vmeanP=mean(vecPS);
+Phat=reshape(vmeanP,q,J);
+
+sortedPnor=sort(vecPS);
+PI_P=sortedPnor([index_L index_U],:); %PI: Posterior Interval
+LPI_P=reshape(PI_P(1,:),q,J); %Lower limit of PI for P
+UPI_P=reshape(PI_P(2,:),q,J); %Upper limit of PI for P
+
+P=Phat*100;
+LL=LPI_P*100; %Lower limit of error bar
+UL=UPI_P*100; %Upper limit of error bar
 x = 1:J;
+
 figure
-if nargin==3
-    for k=1:q
-        subplot(q,1,k)
-        P_bargraph = [P(k,:)];
-        upper_limit = [UL(k,:)-P(k,:)];
-        lower_limit  = [P(k,:)-LL(k,:)];
-        
-        bar(x,P_bargraph )
-        axis auto
-        ylabel(k)
-        hold on     
-        eb = errorbar(x,P_bargraph,lower_limit,upper_limit);
-        grid on
-        %grid minor
-        eb.Color = [0 0 0];
-        eb.LineStyle = 'none';        
-        hold off
-    end
-elseif nargin==4
-    for k=1:q
-        subplot(q,1,k)
-        P_bargraph = [P(k,:)];
-        upper_limit = [UL(k,:)-P(k,:)];
-        lower_limit  = [P(k,:)-LL(k,:)];
-        
-        bar(x,P_bargraph )
+for k=1:q
+    subplot(q,1,k)
+    P_bargraph = [P(k,:)];
+    upper_limit = [UL(k,:)-P(k,:)];
+    lower_limit  = [P(k,:)-LL(k,:)];
+    
+    bar(x,P_bargraph )
+    if nargin==3
         set(gca,'xtick',[1:J],'xticklabel',variable_name)
-        axis auto
-        ylabel(k)
-        hold on       
-        eb = errorbar(x,P_bargraph,lower_limit,upper_limit);
-        grid on
-        %grid minor
-        eb.Color = [0 0 0];
-        eb.LineStyle = 'none';       
-        hold off
     end
+    axis auto
+    ylabel(['Source ',num2str(k)])
+    if k==1
+        title('Source Composition Profiles')
+    elseif k==q
+        xlabel('Species')
+    end
+    hold on
+    eb = errorbar(x,P_bargraph,lower_limit,upper_limit);
+    grid on
+    eb.Color = [0 0 0];
+    eb.LineStyle = 'none';
+    hold off
 end
 %-------------------
 
